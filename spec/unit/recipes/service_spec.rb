da216014ba35e2cc1504a7028755b1666420b10a
cduong13 <chrisduong83@gmail.com>
2017-02-22 11:01:24 +0700

    test start service

---
+++ spec/unit/recipes/service_spec.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: httpd-tdcd
+# Spec:: service
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'httpd-tdcd::service' do
+  context 'When all attributes are default, on an unspecified platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
+
+    it 'start the service' do
+      expect(chef_run).to start_service('httpd')
+    end
+
+  end
+end
