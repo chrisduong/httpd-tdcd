bd2d6fdb6d1cde27507ef0776861b803191c07fc
cduong13 <chrisduong83@gmail.com>
2017-02-27 16:48:00 +0700

    added attribute package_name to recipe

--- recipes/install.rb
+++ recipes/install.rb
@@ -3,4 +3,6 @@
 # Recipe:: install
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-package 'httpd'
+
+# Presentation 7: Testing While Refactoring to Attributes
+package node['httpd']['package_name']
