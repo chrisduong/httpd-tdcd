b5042815fc0a3b61253d8c90dc1d6018f0192eb1
cduong13 <chrisduong83@gmail.com>
2017-02-27 17:05:23 +0700

    same for service_name

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@
 case node['platform']
 when 'ubuntu'
   default['httpd']['package_name'] = 'apache2'
+  default['httpd']['service_name'] = 'apache2'
 else
   default['httpd']['package_name'] = 'httpd'
+  default['httpd']['service_name'] = 'httpd'
 end
-
-default['httpd']['service_name'] = 'httpd'
