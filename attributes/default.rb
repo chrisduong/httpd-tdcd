d8d962d85299e96361332668ec7d8be89d586f20
cduong13 <chrisduong83@gmail.com>
2017-02-27 17:04:15 +0700

    update attribute to support platforms

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,11 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-default['httpd']['package_name'] = 'httpd'
+case node['platform']
+when 'ubuntu'
+  default['httpd']['package_name'] = 'apache2'
+else
+  default['httpd']['package_name'] = 'httpd'
+end
+
 default['httpd']['service_name'] = 'httpd'
